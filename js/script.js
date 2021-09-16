82d7ce20f46a50d92489000f2ae7ff67fd01b55b
kjoh-dev <kevinjoh85@gmail.com>
2021-09-16 14:50:15 -0400

    script.js: Reorganization. Relocated some variables to Game() function from within sub-functions.

--- js/script.js
+++ js/script.js
@@ -6,8 +6,32 @@ A. Inside Game() function:
 
 1. Set playerSelection with player's input using prompt().
 2. Check for input validity using CheckInputValidity().
+3. a) If invalid input, go back to step #1. (Display warning?)
+3. b) If valid input, proceed to next step.
+3. c) If null input, quit game.
+4. Format playerSelection using CapitalizeString().
+5. Get/Set computerSelection using ComputerPlay().
+6. Get round result using PlayRound().
+7. Keep score using (). Until 5 rounds are up.
+8. Display round result using console.log().
+9. a) If 5 rounds are not up, go to step #1.
+9. b) If 5 rounds are up, proceed to the next step.
+10. Declare winner and display final score.
 */
+
 let playerSelection = GetPlayerPlay();
+let computerSelection = ComputerPlay();
+
+const ROCK = "Rock";
+const PAPER = "Paper";
+const SCISSORS = "Scissors";
+const WIN = "Win";
+const LOSE = "Lose";
+const TIE = "Tie";
+
+let playerScore = 0;
+let computerScore = 0;
+let roundResult = PlayRound();
 
 function CheckInputValidity(playerSelection){
     if(playerSelection.toUpperCase() === "ROCK" || playerSelection.toUpperCase() === "PAPER" || playerSelection.toUpperCase() === "SCISSORS"){
@@ -33,9 +57,7 @@ function GetPlayerPlay(){
     }
 }
 
-/*
-3. Format playerSelection using CapitalizeString().
-*/
+
 if (playerSelection ?? true){
     console.log("Quit Game");
 } else {
@@ -54,28 +76,8 @@ const CapitalizeString = (queryString) => {
     }
 }
 
-/*
-4. a) If invalid input, go back to step #1. (Display warning?)
-4. b) If valid input, proceed to step #5.
-5. Get/Set computerSelection using ComputerPlay().
-6. Get round result using PlayRound().
-7. Display round result using console.log().
-8. Keep score using (). Until 5 rounds are up.
-9. a) If 5 rounds are not up, go to step #1.
-9. b) If 5 rounds are up, proceed to the next step.
-10. Declare winner and display final score.
-*/
 
 
-
-
-
-
-/* 1. Make a function called "computerPlay" that returns either "Rock", "Paper" or "Scissors". 
-- There are 3 possible combinations: Rock/Paper/Scissors.
-- Generate a random number between 0 to 99.
-- 0-32=Rock, 33-65=Paper, 66-98=Scissors, 99=reroll.
-*/
 function ComputerPlay() {
     let play;
     let randomNumber = Math.floor(Math.random()*100);
@@ -96,42 +98,10 @@ function ComputerPlay() {
     return play;
 }
 
-/*
-Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 
-    Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-*/
-/*
-1. Input Variables: playerSelection = string, case-insensitive; computerSelection = string.
-*/
-let computerSelection = ComputerPlay();
+function PlayRound(){
 
-
-
-/*
-Play outcomes:
-1. Player plays Rock against Computer's:
- - Rock = Tie,
- - Paper = Player Lose,
- - Scissors = Player Win.
-2. Player plays Paper against Computer's:
- - Rock = Player Win,
- - Paper = Tie,
- - Scissors = Player Lose.
-3. Player plays Scissors against Computer's:
- - Rock = Player Lose,
- - Paper = Player Win,
- - Scissors = Tie. 
-*/
-function PlayRound(playerSelection, computerSelection){
-    const ROCK = "Rock";
-    const PAPER = "Paper";
-    const SCISSORS = "Scissors";
-    const WIN = "Win";
-    const LOSE = "Lose";
-    const TIE = "Tie";
-
-    let outcome;
+    const outcome;
     switch(true){
         case (playerSelection === ROCK && computerSelection === ROCK):
             outcome = TIE;
@@ -174,9 +144,18 @@ function PlayRound(playerSelection, computerSelection){
     }
 }
 
-console.log(PlayRound(playerSelection, computerSelection));
+
 /*
-2. Output Variable: string that incorporates 1) win/loss, 2) player and computer selections (inputs).
+function UpdateScore(){
+
+}
+
+function ShowResult (result){
+    console.log(result);
+}
+function ShowCurrentScore (score){
+    console.log(score);
+}
 */
 
 
