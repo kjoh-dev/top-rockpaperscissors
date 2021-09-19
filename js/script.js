bee61a1ebfda7167f63bea6476e1eb456fdbd1cc
kjoh-dev <kevinjoh85@gmail.com>
2021-09-19 17:46:52 -0400

    index: restructured by hiding header (for future implementation), adding <main>, VS <div> and computer move image. style: implemented initial rules. script: implemented button events for player move selection and layout transition to show computer move.

--- js/script.js
+++ js/script.js
@@ -1,196 +1,259 @@
-/*
-Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. 
-- Use prompt() to get input from the user.
+/*Vision:
+TOP Requirements:
+- Buttons for player moves: rock, paper and scissors.
+- Display results: running score and announce winner (player/computer to win 5 rounds).
+Additional Considerations and Organization:
+A. Title Screen:
+    1. Title
+    2. Graphic
+    3. Start Button
+B. Player Move Screen:
+    1. Show Header with Player Score and Computer Score.
+    2. Show Buttons for each move: Rock, Paper and Scissors
+    3. Show Graphic representing each move
+    4. Selected graphic movement to center-left while the rest fade out.
+    5. Computer move selection: show selection animation.
+    6. Show round result. Update scores.
+    7. Show final score and leader if applicable. Else, go to next round (repeat B).
+C. Game Over Screen:
+    1. Announce winner. (Center and move to top?)
+    2. Show final scores and number of rounds taken.
+    3. Show button for Play Again.
 
-A. Inside Game() function:
-
-1. Set playerSelection with player's input using prompt().
-2. Check for input validity using CheckInputValidity().
-3. a) If invalid input, go back to step #1. (Display warning?)
-3. b) If valid input, proceed to next step.
-3. c) If null input, quit game.
-4. Format playerSelection using CapitalizeString().
-5. Get/Set computerSelection using ComputerPlay().
-6. Get round result using PlayRound().
-7. Keep score using (). Until 5 rounds are up.
-8. Display round result using console.log().
-9. a) If 5 rounds are not up, go to step #1.
-9. b) If 5 rounds are up, proceed to the next step.
-10. Declare winner and display final score.
 */
-Game();
 
-function Game(){
-    
-    const ROCK = "Rock";
-    const PAPER = "Paper";
-    const SCISSORS = "Scissors";
-    const WIN = "Win";
-    const LOSE = "Lose";
-    const TIE = "Tie";
-    
-    const ROUNDSPERGAME = 5;
-    let roundNumber = 1;
-    let playerScore = 0;
-    let computerScore = 0;
-    let roundResult = null; 
-    
-    let playerSelection = null;
-    let computerSelection = null;
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const buttonsContainer = document.querySelector("#buttons-container");
 
-    for (i = 1; i <= ROUNDSPERGAME; i++){
-        GetPlayerPlay();
-        if (playerSelection === null){
-            console.log("Quit Game");
-            return;
-        } 
-        playerSelection = CapitalizeString(playerSelection);
+const PLAYER_DEFAULT = "player-default";
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
 
-        computerSelection = ComputerPlay();
+let buttonsHidden = false;
+let playerSelection = null;
+let computerSelection = null;
+let roundResult = null; 
+let roundNumber = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-        roundNumber = i;
-        roundResult = PlayRound();
-        UpdateScore();
-        ShowRoundResult();
-        ShowScore();
+for (let i = 0; i < buttonsContainer.childElementCount; i++) {
+    const button = buttonsContainer.children[i];
+    button.addEventListener("mouseenter", showPlayerMoveImage);
+    button.addEventListener("mouseleave", showPlayerMoveImage);
+    button.addEventListener("click", setPlayerMove);
+    button.addEventListener("click", togglePlayerMoveButtons);
+    button.addEventListener("click", showComputerMoveLayout);
+
+}
+
+//Run after computer move layout transition complete:
+//computerPlay();
+// calcRound();
+// calcScore();
+
+//Need to be accompanied by relevant html elements before full implementation:
+//showRoundResult()
+// showScore()
+
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random()*100);
+    switch(true){
+        case randomNumber>98:
+            computerSelection = ComputerPlay();
+            break;
+        case randomNumber>65:
+            computerSelection = SCISSORS;
+            break;
+        case  randomNumber>32:
+            computerSelection = PAPER;
+            break;
+        default:
+            computerSelection = ROCK;
+            break;
     }
+}
 
-
-
-    function CheckInputValidity(){
-        if(playerSelection === null){
-            return "quit";
-        } else if (playerSelection.toUpperCase() === "ROCK" || playerSelection.toUpperCase() === "PAPER" || playerSelection.toUpperCase() === "SCISSORS"){
-            return "valid";
-        } else {
-            return "invalid";
-        }
+function calcRound(){
+    roundNumber++;
+    switch(true){
+        case (playerSelection === ROCK && computerSelection === ROCK):
+            roundResult = TIE;
+            break;
+        case (playerSelection === ROCK && computerSelection === PAPER):
+            roundResult = LOSE;
+            break;
+        case (playerSelection === ROCK && computerSelection === SCISSORS):
+            roundResult = WIN;
+            break;
+        case (playerSelection === PAPER && computerSelection === ROCK):
+            roundResult = WIN;
+            break;
+        case (playerSelection === PAPER && computerSelection === PAPER):
+            roundResult = TIE;
+            break;
+        case (playerSelection === PAPER && computerSelection === SCISSORS):
+            roundResult = LOSE;
+            break;
+        case (playerSelection === SCISSORS && computerSelection === ROCK):
+            roundResult = LOSE;
+            break;
+        case (playerSelection === SCISSORS && computerSelection === PAPER):
+            roundResult = WIN;
+            break;
+        case (playerSelection === SCISSORS && computerSelection === SCISSORS):
+            roundResult = TIE;
+            break;
+        default:
+            console.log(`Error - Unaccounted for combo. Player's ${playerSelection} vs. Computer's ${computerSelection}.`)
+            break;
     }
+}
 
-    function GetPlayerPlay(){
-        playerSelection = prompt("What's your play? Rock/Paper/Scissors", "");
-        let inputValidity = CheckInputValidity(playerSelection);
-        if (inputValidity === "valid"){
-            return;
-        } else if (inputValidity === "invalid"){
-            GetPlayerPlay();
-        } else {
-            playerSelection = null;
-        }
+function calcScore(){
+    if(roundResult === WIN){
+        ++playerScore;
+    } 
+    
+    if(roundResult === LOSE){
+        ++computerScore;
     }
+}
+
+function showRoundResult(){
+    if(roundResult === WIN){
+        alert(`You Win! ${playerSelection} beats ${computerSelection}.`);
+    } else if(roundResult === LOSE){
+        alert(`You Lose! ${playerSelection} loses to ${computerSelection}.`);
+    } else {
+        alert(`It's a Tie! ${playerSelection} ties ${computerSelection}.`);
+    }
+}
+
+function showScore(){
+
+    alert(`End of Round ${roundNumber}`);
+    alert("Scores:");
+    alert(`Player: ${playerScore}`);
+    alert(`Computer: ${computerScore}`);
+    if(playerScore > computerScore){
+        alert("Player is Winning!");
+    } else if (playerScore < computerScore){
+        alert("Computer is Winning!");
+    } else{
+        alert("Scores are Tied!");
+    }
+}
+
+function setPlayerMove(e){
+    const move = e.target.className;
+    switch (true) {
+        case (move === ROCK):
+            playerSelection = ROCK;
+            break;
+        case (move === PAPER):
+            playerSelection = PAPER;
+            break;
+        case (move === SCISSORS):
+            playerSelection = SCISSORS;
+            break;
+        default:
+            alert(`Error - Unexpected play choice: ${move}`);
+            break;
+    }
+}
+
+function showMoveImage(move, imageElement){
+
+    switch (true) {
+        case (move === PLAYER_DEFAULT):
+            imageElement.src = "img/rps-all.png";
+            break;
+        case (move === ROCK):
+            imageElement.src = "img/rps-rock.png";
+            break;
+        case (move === PAPER):
+            imageElement.src = "img/rps-paper.png";
+            break;
+        case (move === SCISSORS):
+            imageElement.src = "img/rps-scissors.png";
+            break;
+        default:
+            break;
+    }
+}
+
+function toggleComputerMoveImage(){
+    const computerMoveImage = document.querySelector("#computer-move-image");
+    const computedStyle = window.getComputedStyle(computerMoveImage);
+    const displayValue = computedStyle.getPropertyValue("display");
+    if(displayValue === "none"){
+        computerMoveImage.style.display = "block";
+    }
+    else{
+        computerMoveImage.style.display = "none";
+    }
+}
+
+function toggleVS(){
+    const vs = document.querySelector("#vs");
+    const computedStyle = window.getComputedStyle(vs);
+    const displayValue = computedStyle.getPropertyValue("display");
+    if(displayValue === "none"){
+        vs.style.display = "block";
+    }
+    else{
+        vs.style.display = "none";
+    }
+}
+
+function showPlayerMoveImage(e){
+    const playerMoveImage = document.querySelector("#player-move-image");
+
+    if (e.type === "mouseleave" && buttonsHidden !== true) {
+        showMoveImage(PLAYER_DEFAULT, playerMoveImage);
+    } else {
+        showMoveImage(e.target.className, playerMoveImage);
+    }
+    //Hide buttons after player move selection
+    if (e.type === "click") {
+        buttonsContainer.style.display = "none";
+        buttonsHidden = true;
+    }
+}
+
+//Toggle player move buttons on/off.
+function togglePlayerMoveButtons(e){
+    if(buttonsHidden === true){
+        buttonsContainer.style.display = "flex";
+        buttonsHidden = false;    
+    } else {
+        buttonsContainer.style.display = "none";
+        buttonsHidden = true;    
+    }
+}
+
+//Toggles computer move screen layout on/off.
+function showComputerMoveLayout(){
+    const mainElement = document.querySelector("main");
+    const compStyles = window.getComputedStyle(mainElement);
+
+    if (compStyles.getPropertyValue("flex-direction") === "column") {
+        mainElement.style.flexDirection = "row";
+        mainElement.style.justifyContent = "center";
+        mainElement.style.alignItems = "start";
         
-    function CapitalizeString(queryString){
-        if(typeof queryString !== "string"){
-            return "";
-        }
-        else if(queryString.length < 2){
-            return queryString.toUpperCase();
-        }
-        else{
-            return queryString.charAt(0).toUpperCase() + queryString.slice(1).toLowerCase();
-        }
-    }
-    
-    function ComputerPlay() {
-        let play;
-        let randomNumber = Math.floor(Math.random()*100);
-        switch(true){
-            case randomNumber>98:
-                play = ComputerPlay();
-                break;
-            case randomNumber>65:
-                play = "Scissors";
-                break;
-            case  randomNumber>32:
-                play = "Paper";
-                break;
-            default:
-                play = "Rock";
-                break;
-        }
-        return play;
-    }
-        
-    function PlayRound(){
-    
-        let outcome;
-        switch(true){
-            case (playerSelection === ROCK && computerSelection === ROCK):
-                outcome = TIE;
-                break;
-            case (playerSelection === ROCK && computerSelection === PAPER):
-                outcome = LOSE;
-                break;
-            case (playerSelection === ROCK && computerSelection === SCISSORS):
-                outcome = WIN;
-                break;
-            case (playerSelection === PAPER && computerSelection === ROCK):
-                outcome = WIN;
-                break;
-            case (playerSelection === PAPER && computerSelection === PAPER):
-                outcome = TIE;
-                break;
-            case (playerSelection === PAPER && computerSelection === SCISSORS):
-                outcome = LOSE;
-                break;
-            case (playerSelection === SCISSORS && computerSelection === ROCK):
-                outcome = LOSE;
-                break;
-            case (playerSelection === SCISSORS && computerSelection === PAPER):
-                outcome = WIN;
-                break;
-            case (playerSelection === SCISSORS && computerSelection === SCISSORS):
-                outcome = TIE;
-                break;
-            default:
-                console.log(`Error - Unaccounted for combo. Player's ${playerSelection} vs. Computer's ${computerSelection}.`)
-                break;
-        }
-        
-        return outcome;
-    }
+        toggleVS();
+        toggleComputerMoveImage();
+    } else {
+        mainElement.style.flexDirection = "column";
+        mainElement.style.justifyContent = "start";
+        mainElement.style.alignItems = "center";
 
-    function UpdateScore(){
-        if(roundResult === WIN){
-            ++playerScore;
-        } 
-        
-        if(roundResult === LOSE){
-            ++computerScore;
-        }
+        toggleVS();
+        toggleComputerMoveImage();
     }
-
-    function ShowRoundResult(){
-        if(roundResult === WIN){
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}.`);
-        } else if(roundResult === LOSE){
-            console.log(`You Lose! ${playerSelection} loses to ${computerSelection}.`);
-        } else {
-            console.log(`It's a Tie! ${playerSelection} ties ${computerSelection}.`);
-        }
-    }
-
-    function ShowScore(){
-        let heading;
-        let finalWinner;
-        if(roundNumber < ROUNDSPERGAME){
-            heading = `Round ${roundNumber} Score`;
-            finalWinner = "";
-        }
-        else{
-            heading = `Final Score`;
-            if(playerScore > computerScore){
-                finalWinner = "Player Wins!";
-            } else if (playerScore < computerScore){
-                finalWinner = "Computer Wins!";
-            } else{
-                finalWinner = "Tied. You both Lose!"
-            }
-        }
-        console.log(heading);
-        console.log(`Player: ${playerScore}`);
-        console.log(`Computer: ${computerScore}`);
-        console.log(finalWinner);
-    }
-
 }
